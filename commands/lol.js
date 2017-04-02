a51dc65f447b765c53b61c110816ca6dcfd77049
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2017-04-02 11:53:18 -0700

    Removed togglelevel.js
    Temporarily removed old, useless file.

--- commands/lol.js
+++ commands/lol.js
@@ -1,7 +1,7 @@
 const lols = ['It smells like someone sneezed!', 'You may not rest now, there is a dance off nearby!', 'WUT? I CAN\'T HEAR YOU OVER MY CAPS!', 'Just remember, if the world didn\'t suck, we\'d all fall off!', '\"You\'re a unit of power, Harry.\" \"I\'m a watt?"', 'I\'d tell you a joke about Unix, but I don\'t have permission.', 'Computers are like air conditioning. They both become useless after you open Windows.', 'Learning about spheres is pointless.', 'There\'s a fine line between a numerator and a denominator.', 'It smells like someone sneezed!'];
 
 exports.run = (client, message) => {
-  message.channel.sendMessage(`${lols[Math.floor(Math.random() * lols.length)]}`).catch(err => console.log(err.stack, "error"));
+  message.channel.sendMessage(`${lols[Math.floor(Math.random() * lols.length)]}`).catch(err => console.log(err.stack));
 };
 
 exports.conf = {
