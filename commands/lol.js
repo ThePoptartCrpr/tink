fa13bb3d1ef35e9fe94112f3378f2162e020fe1b
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2017-04-02 11:51:27 -0700

    Changed lol.js
    Reworked randomizer system to be more efficient + easy to work with.

--- commands/lol.js
+++ commands/lol.js
@@ -1,45 +1,7 @@
+const lols = ['It smells like someone sneezed!', 'You may not rest now, there is a dance off nearby!', 'WUT? I CAN\'T HEAR YOU OVER MY CAPS!', 'Just remember, if the world didn\'t suck, we\'d all fall off!', '\"You\'re a unit of power, Harry.\" \"I\'m a watt?"', 'I\'d tell you a joke about Unix, but I don\'t have permission.', 'Computers are like air conditioning. They both become useless after you open Windows.', 'Learning about spheres is pointless.', 'There\'s a fine line between a numerator and a denominator.', 'It smells like someone sneezed!'];
+
 exports.run = (client, message) => {
-  let lolRandom = Math.floor(Math.random() * 10);
-  	  if (lolRandom === 0) {
-  		  message.channel.sendMessage('It smells like someone sneezed!');
-  	  } else
-
-  	  if (lolRandom === 1) {
-  		  message.channel.sendMessage('You may not rest now, there is a dance off nearby!');
-  	  } else
-
-  	  if (lolRandom === 2) {
-  		  message.channel.sendMessage(`WUT? I CAN'T HEAR YOU OVER MY CAPS!`);
-  	  } else
-
-  	  if (lolRandom === 3) {
-  		  message.channel.sendMessage(`Just remember, if the world didn't suck, we'd all fall off!`);
-  	  } else
-
-  	  if (lolRandom === 4) {
-  		  message.channel.sendMessage(`"You're a unit of power, Harry." "I'm a watt?"`);
-  	  } else
-
-  	  if (lolRandom === 5) {
-  		  message.channel.sendMessage(`I'd tell you a joke about Unix, but I don't have permission.`);
-  	  } else
-
-  	  if (lolRandom === 6) {
-  		  message.channel.sendMessage(`Computers are like air conditioning. They both become useless after you open Windows.`);
-  	  } else
-
-  	  if (lolRandom === 7) {
-  		  message.channel.sendMessage(`Learning about spheres is pointless.`);
-  	  } else
-
-  	  if (lolRandom === 8) {
-  		  message.channel.sendMessage(`There's a fine line between a numerator and a denominator.`);
-  	  }	else
-
-  	  if (lolRandom === 9) {
-  		  message.channel.sendMessage(`It smells like someone sneezed!`);
-  	  }	else {
-  	  }
+  message.channel.sendMessage(`${lols[Math.floor(Math.random() * lols.length)]}`).catch(err => console.log(err.stack, "error"));
 };
 
 exports.conf = {
