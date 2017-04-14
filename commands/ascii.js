1e59c3afe47c6ded65baf62d3ca0142da135a06c
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2017-04-14 09:25:19 -0700

    Added ascii command

---
+++ commands/ascii.js
@@ -0,0 +1,20 @@
+const figlet = require('figlet');
+exports.run = (client, message, [...text]) => {
+  figlet(text.join(" "), (err, data) => {
+    if (err) return console.log(err);
+    return message.channel.sendCode("", data).catch(e => console.log(e));
+});
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['banner', 'asciitext'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'ascii',
+  description: 'Generates an ASCII text banner.',
+  usage: '+ascii <text>'
+};
