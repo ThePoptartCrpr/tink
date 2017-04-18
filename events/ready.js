592eace163a2d67bb213b42ddcc38dd84ef8c1c8
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2017-04-18 16:59:17 -0700

    Posting stats to discordbots.org

--- events/ready.js
+++ events/ready.js
@@ -8,11 +8,14 @@ module.exports = client => {
 let totalGuild = client.guilds.size;
 let botsDiscordPwToken = config.botsDiscordPwToken;
 let botsDiscordPwUser = config.botsDiscordPwUser;
+let discordBotsList = config.discordBotsListToken;
+let discordBotsListUser = config.discordBotsListUser;
 
   const updateBotsDiscordPw = function(totalGuild) {
     if (botsDiscordPwToken && botsDiscordPwUser) {
       console.log(`Updating.`)
       console.log(`Tink is on ${totalGuild} servers.`);
+      console.log('Posting stats to bots.discord.pw.');
       const requestBody = {
         method: 'POST',
         url: `https://bots.discord.pw/api/bots/${botsDiscordPwUser}/stats`,
@@ -35,6 +38,34 @@ let botsDiscordPwUser = config.botsDiscordPwUser;
     }
   };
 
+  const updateDiscordBotsList = function(totalGuild) {
+    if (botsDiscordPwToken && botsDiscordPwUser) {
+      // console.log(`Updating.`)
+      // console.log(`Tink is on ${totalGuild} servers.`);
+      console.log('Posting stats to discordbots.org.');
+      const requestBody = {
+        method: 'POST',
+        url: `https://discordbots.org/api/bots/${discordBotsListUser}/stats`,
+        headers: {
+          Authorization: discordBotsList,
+          'Content-Type': 'application/json',
+        },
+        body: {
+          // shard_id: parseInt(this.shardId, 10),
+          // shard_count: parseInt(this.shardCount, 10),
+          server_count: parseInt(totalGuild, 10),
+        },
+        json: true,
+      };
+      request(requestBody)
+      .then((parsedBody) => {
+        console.log(parsedBody);
+      })
+      .catch(error => console.log(error));
+    }
+  };
+
   updateBotsDiscordPw(totalGuild);
+  updateDiscordBotsList(totalGuild);
   client.user.setGame(`${totalGuild} servers | +help`);
 };
