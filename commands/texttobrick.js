864c2bb1ba4f003af7ee1ee36c32ddeee7d90a4f
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2017-04-19 10:08:14 -0700

    Added texttobrick command

---
+++ commands/texttobrick.js
@@ -0,0 +1,17 @@
+exports.run = (client, message, [...text]) => {
+  text = text.join(" ").replace(/ /g, "   ").replace(/[a-z]/g, ":regional_indicator_$&:");
+  message.channel.sendMessage(text);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['brick', 'ttb'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'texttobrick',
+  description: 'Generates brick text.',
+  usage: '+texttobrick <text>'
+};
