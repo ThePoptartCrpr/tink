28ed5299117d58366595281a5cd9bcf3340e8b4a
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2017-04-28 10:46:47 -0700

    Added catch failsafe for lack of permissions in purge

--- commands/purge.js
+++ commands/purge.js
@@ -2,7 +2,7 @@ exports.run = function(client, message, args) {
   let messagecount = parseInt(args.join(' '));
   message.channel.fetchMessages({
     limit: messagecount
-  }).then(messages => message.channel.bulkDelete(messages));
+  }).then(messages => message.channel.bulkDelete(messages)).catch(err => message.reply('I do not have permission to delete some of those messages!'));
 };
 
 exports.conf = {
